f4ace1e1a55279c8aa223162d4c0e6921280ea2f
NicolasAguilar-prog <naguilarurgiles@gmail.com>
2026-04-17 10:21:53 -0500

    UPD: Constante,Lineal

--- src/complejidades/ComplejidadConstante.java
+++ src/complejidades/ComplejidadConstante.java
@@ -1,5 +1,19 @@
 package complejidades;
 
+/*
+     * Complejidad: O(1)
+     * Razón: Siempre se ejecutan las mismas instrucciones,
+     * no importa el tamaño de los datos.
+*/
+
 public class ComplejidadConstante {
-    
+    public void ejemplo() {
+        System.out.println("Ejemplo O(1):");
+
+        int a = 10;
+        int b = 20;
+        int suma = a + b;
+
+        System.out.println("La suma es: " + suma);
+    }
 }
--- src/complejidades/ComplejidadLineal.java
+++ src/complejidades/ComplejidadLineal.java
@@ -1,5 +1,19 @@
 package complejidades;
 
 public class ComplejidadLineal {
-    
+
+    /**
+     * Complejidad: O(n)
+     * Razón: El tiempo de ejecución depende directamente
+     * del tamaño del arreglo.
+     */
+    public void ejemplo() {
+        System.out.println("Ejemplo O(n):");
+
+        int[] numeros = {1, 2, 3, 4, 5};
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Número: " + numeros[i]);
+        }
+    }
 }
