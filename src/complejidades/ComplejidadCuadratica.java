1ed258b72916d18962c1e4646c50721d8ac76978
Ricardo Uzhca <ricardouzhcab27@gmail.com>
2026-04-19 20:59:15 -0500

    UPD: C. Cuadratica y Logaritmica

--- src/complejidades/ComplejidadCuadratica.java
+++ src/complejidades/ComplejidadCuadratica.java
@@ -1,5 +1,20 @@
 package complejidades;
 
+/* Complejidad Cuadrática: O(n^2)
+    Razón: Ocurre cuando el tiempo de ejecución crece al cuadrado del 
+    tamaño de la entrada. Esto sucede principalmente cuando hay bucles 
+    anidados (un ciclo dentro de otro). Se vuelve ineficiente con muchos 
+    datos porque el número de tareas aumenta drásticamente.
+ */
 public class ComplejidadCuadratica {
-
+    public void ejemploCuadratico() {
+        System.out.println("Ejemplo O(n^2):");
+        int n = 3; // Tamaño de la entrada
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.println("i: " + i + ", j: " + j);
+            }
+        }
+    }   
 }
--- src/complejidades/ComplejidadLogaritmica.java
+++ src/complejidades/ComplejidadLogaritmica.java
@@ -1,5 +1,20 @@
 package complejidades;
 
+/* Complejidad Logarítmica: O(log n)
+    Razón: Ocurre cuando el algoritmo divide el problema a la mitad 
+    en cada paso. Es muy eficiente porque, a medida que aumentan los 
+    datos, el número de operaciones crece muy poco, permitiendo manejar 
+    grandes volúmenes de información rápidamente.
+*/
+
 public class ComplejidadLogaritmica {
-    
+    public void ejemploLogaritmico() {
+        System.out.println("Ejemplo O(log n):");
+        int n = 32; // Empezamos con 32 elementos
+        
+        //i se divide entre 2 en cada vuelta
+        for (int i = n ; i>1 ; i = i/2) {
+            System.out.println("Dividiendo... ahora quedan: " + i);
+        }
+    }
 }
