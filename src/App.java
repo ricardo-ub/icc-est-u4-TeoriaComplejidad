869e3c2f53e222e86ba8c02f9b269c974a9ce665
NicolasAguilar-prog <naguilarurgiles@gmail.com>
2026-04-24 09:10:42 -0500

    UPD: FINAL FINALISIMO

--- src/App.java
+++ src/App.java
@@ -1,5 +1,31 @@
+import complejidades.ComplejidadConstante;
+import complejidades.ComplejidadLineal;
+import complejidades.ComplejidadCuadratica;
+import complejidades.ComplejidadLogaritmica;
+import complejidades.ComplejidadNLogN;
+import complejidades.ComplejidadPeorCaso;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("TODAS LAS COMPLEJIDADES: ");
+        System.out.println("");
+
+        ComplejidadConstante c1 = new ComplejidadConstante();
+        c1.ejemplo();
+
+        ComplejidadLineal c2 = new ComplejidadLineal();
+        c2.ejemplo();
+
+        ComplejidadCuadratica c3 = new ComplejidadCuadratica();
+        c3.ejemploCuadratico();
+
+        ComplejidadLogaritmica c4 = new ComplejidadLogaritmica();
+        c4.ejemploLogaritmico();
+
+        ComplejidadNLogN c5 = new ComplejidadNLogN();
+        c5.ejemploNLogN();
+
+        ComplejidadPeorCaso c6 = new ComplejidadPeorCaso();
+        c6.ejemploNLogN();
     }
 }
